47dade4f1418e7475ebed24ea38eb60251719201
Pietro907 <pietrop.puri@gmail.com>
2023-10-23 15:14:43 +0200

    add recharge repo

---
+++ assets/readme/track2.md
@@ -0,0 +1,15 @@
+<!-- 
+
+Continuare a lavorare nella stessa repo di ieri aggiungendo almeno:
+
+- un trait; 
+
+ed 
+
+- un exception;
+
+
+ al vostro shop!
+
+
+ -->
--- index.php
+++ index.php
@@ -26,30 +26,35 @@ class Prodotti
 
 class Categorie
 {
-
     public $categoria;
-    public $canide;
-    public $felino;
 
     public function __construct($categoria)
     {
-
         $this->categoria = $categoria;
+    }
+
+    public function get_categ() {
+        return $this.$categoria->categoria;
     }
 }
 
 
 $cani = new Prodotti('Monge All Breeds Adult Salmone e Riso', '€59,99', './assets/img/Monge-All-Breeds-Adult-Salmone-e-Riso-12Kg.webp', new Categorie('Cane'), 'fa-solid fa-dog', 'Quantità: 12KG');
 $cani2 = new Prodotti('Virtus Dog Adult Rustic', '€26,99', './assets/img/virtus-rustic-cane-adult.webp', new Categorie('Cane'), 'fa-solid fa-dog', 'Quantità: 2KG');
-$cani3 = new Prodotti('Acana Classic Red', '€24,99', './assets/img/acana-dog-classic-red.webp', new Categorie('Cane'), 'fa-solid fa-dog', 'Quantità: 2kg');
+$cani3 = new Prodotti('Acana Classic Red', '€24,99', './assets/img/acana-dog-classic-red.webp', new Categorie('Cane'), 'fa-solid fa-dog', 'Quantità: 2KG');
+
 
 $gatti = new Prodotti('Purina ONE Adult Pollo & Cereali integrali Crocchette', '€15,29', './assets/img/288303_mhi_purinaone_adult_huhn_vollkorngetreide_hs_01_7.jpg', new Categorie('Gatto'), 'fa-solid fa-cat', 'Quantità: 2,8KG');
 $gatti2 = new Prodotti('Expecial Cat Complete Pollo', '€39,29', './assets/img/Expecial-Gatto-Adulto-Pollo--2-.webp', new Categorie('Gatto'), 'fa-solid fa-cat', 'Quantità: 10KG');
 $gatti3 = new Prodotti('Ultima Cat Pollo', '€39,99', './assets/img/ultima-cat-pollo.webp', new Categorie('Gatto'), 'fa-solid fa-cat', 'Quantità: 7,5KG');
 
-$articoliArray = array($cani, $gatti, $cani2, $gatti2, $cani3, $gatti3);
+$articoliArray = [$cani, $gatti, $cani2, $gatti2, $cani3, $gatti3];
+//var_dump($articoliArray);
 
-$result = $cani->categorie?->categoria;
+$result = $categoria->$categorie?->categoria;
+
+var_dump($categoria->categorie?->categoria);
+
 
 ?>
 
